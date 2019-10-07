5884a70f96c3f8820b442ec5e97765b20473f632
zlataya <yana.youngfunny@gmail.com>
2019-10-07 23:10:28 +0300

    Added logging

--- parser.py
+++ parser.py
@@ -1,20 +1,26 @@
-import os
 import csv
+import glob
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def parse_files(path):
     x_y, x = dict(), list()
-    for rout, dirs, files in os.walk(path):
-        for file in files:
-            with open(rout + file) as f:
-                f_lines = f.readlines()
-                raw_data = [c.split() for c in f_lines]
-                x_y[file] = [{'x': int(float(a)), 'y': b.replace('.', ',')} for a, b in raw_data]
-                x.extend(sorted(set([i['x'] for i in x_y[file]])))
+    for file in glob.glob('%s/*.txt' % path):
+        with open(file) as f:
+            f_name = os.path.basename(file)
+            f_lines = f.readlines()
+            raw_data = [c.split() for c in f_lines]
+            x_y[f_name] = [{'x': int(float(a)), 'y': b.replace('.', ',')} for a, b in raw_data]
+            x.extend(sorted(set([i['x'] for i in x_y[f_name]])))
+            logger.log(20, 'Parsed file %s' % f.name)
     return x, x_y
 
 
-x, x_y = parse_files('recources/')
+x, x_y = parse_files('resources')
 result_dict = {i: [None] * len(x_y) for i in x}
 files = list()
 for n, k in enumerate(x_y):
@@ -24,7 +30,8 @@ for n, k in enumerate(x_y):
 
 redult_set = [(k, ) + tuple(v) for k, v in result_dict.items()]
 
-with open('result.csv', 'w') as fp:
+with open('result.csv', 'w', encoding='utf-8') as fp:
     writer = csv.writer(fp, dialect='excel', lineterminator='\n')
     writer.writerow(['x'] + ['y_(%s)' % f for f in files])
     writer.writerows(sorted(redult_set, key=lambda s: s[0]))
+    logger.log(20, 'Finished writing to file %s' % fp.name)
